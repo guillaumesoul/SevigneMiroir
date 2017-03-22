7b4043652921896bccb74343f8112434258f34b0
guillaumesoullard <guillaume.soullard@gmail.com>
2017-03-22 23:05:59 +0100

    init routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,14 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function addPresentationAction()
+    {
+        return $this->render('default/add.html.twig', array(
+            'variable_name' => 'variable_value',
+        ));
+    }
 }
