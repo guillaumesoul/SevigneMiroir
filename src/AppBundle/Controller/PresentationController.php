3bedecc9cfcc0866da482cdefa73f7666f18b144
guillaumesoullard <guillaume.soullard@gmail.com>
2017-03-25 14:34:37 +0100

    ajout assets and init drap and drop sortable

--- src/AppBundle/Controller/PresentationController.php
+++ src/AppBundle/Controller/PresentationController.php
@@ -11,6 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 class PresentationController extends Controller
 {
 
+
+    /**
+     * permet de lister toutes les présentations enregistrées en base de données
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
@@ -21,6 +27,12 @@ class PresentationController extends Controller
         ]);
     }
 
+
+    /**
+     * permet d'ajouter une nouvelle présentation
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function addAction(Request $request)
     {
         $presentation = new Presentation();
@@ -43,8 +55,22 @@ class PresentationController extends Controller
 
     }
 
+
+    /**
+     * permet d'organiser les présentations
+     * définir les présentations actives
+     * faire des groupes de présentations (un groupe comprenant plusieurs présentations qui vont défiler les unes après les autres)
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function manageAction()
     {
-        return $this->render('presentation/manage.html.twig');
+        //il faut que je liste les présentations enregistrées
+        $em = $this->getDoctrine()->getManager();
+        $presentations = $em->getRepository('AppBundle:Presentation')->findAll();
+
+        return $this->render('presentation/manage.html.twig', [
+            'presentations' => $presentations
+        ]);
     }
 }
