89cc1b0bed95e8a40965af6dd25977c96b8cbaef
guillaumesoullard <guillaume.soullard@gmail.com>
2017-04-14 17:14:51 +0200

    amélioration mise en forme

--- src/AppBundle/Form/SerieType.php
+++ src/AppBundle/Form/SerieType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -15,10 +16,12 @@ class SerieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, array(
+                'label' => 'Nom'
+            ))
             ->add('heureDebut')
             ->add('heureFin')
-            ->add('save', SubmitType::class, array('label' => 'Save'));
+            ->add('save', SubmitType::class, array('label' => 'Enregistrer'));
     }
     
     /**
