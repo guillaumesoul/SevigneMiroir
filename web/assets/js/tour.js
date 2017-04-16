bbb39085d0c97a034bbe00f11dd9fdd9a3c550e3
guillaumesoullard <guillaume.soullard@gmail.com>
2017-04-16 18:55:38 +0200

    tour in progress

--- web/assets/js/tour.js
+++ web/assets/js/tour.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
         case '':
             var enjoyhint_script_steps = [
                 {
-                    'next .mdl-layout__header' : 'Bienvenue sur votre application de gestion de vos affichages',
+                    'next .mdl-layout__header' : 'Bienvenue sur votre application de gestion de vos affichages. Cette application vous permet de créer des listes de présentations Google slide sur des créneaux horaires qui défileront automatiquement',
                 },
                 {
-                    'next #presentation-link' : 'Le menu présentations vous permet de gérer vos présentations (ajouter, modifier, supprimer)'
+                    'next #presentation-link' : 'Le menu présentations vous permet de gérer vos présentations google slide : ajouter, modifier, supprimer'
                 },
                 {
-                    'next #serie-link' : 'Le menu créneaux horaires vous permet de gérer vos créneaux horaires (ajouter, modifier, supprimer)'
+                    'next #serie-link' : 'Le menu créneaux horaires vous permet de gérer vos créneaux horaires : déterminer leur heure de début et heure de fin'
                 },
                 {
                     'next #previsualisation-link' : 'Le menu prévisualisation vous permet  d\'avoir un apercu du rendu final'
@@ -30,12 +30,16 @@ $(document).ready(function() {
                     selector:'#presentation-link',
                     event:'presentation',
                     event_type:'custom',
-                    description:'Cliquez maintenant sur le menu présentations pour gérer vos présentations (ajouter, modifier, supprimer)',
+                    description:'Cliquez maintenant sur le menu présentations',
+                    shape: 'circle'
                 }
             ];
             break;
         case 'presentation':
             var enjoyhint_script_steps = [
+                {
+                  'next #presentation-list' : 'Vos présentations déjà enregistrées sont listées ici'
+                },
                 {
                     onBeforeStart: function(){
                         $('#presentation-add').click(function(e){
@@ -69,14 +73,73 @@ $(document).ready(function() {
                     'next #appbundle_presentation_slideTransitionDuration' : 'facultatif : saisissez la durée de transition en secondes entre chaque slide'
                 },
                 {
-                    'next #presentation-duree' : 'facultatif : saisissez la durée totale de votre présentation'
+                    'next #presentation-duree' : 'facultatif : saisissez la durée totale de votre présentation. Si vou ne saisissez rien, celle-ci sera automatiquement calculée en fonction du nombre de slides et de leurs durées d\'affichage'
                 },
                 {
                     'next #appbundle_presentation_save' : 'Enregistrez votre présentation'
+                },
+                {
+                    onBeforeStart: function(){
+                        $('#serie-link').click(function(e){
+                            enjoyhint_instance.trigger('serie-link');
+                        });
+                    },
+                    selector:'#serie-link',
+                    event:'serie-link',
+                    event_type:'custom',
+                    description:'Cliquez maintenant sur le menu créneaux horaires',
+                    shape: 'circle'
                 }
             ];
             break;
+        case 'serie':
+            var enjoyhint_script_steps = [
+                {
+                    'next #serie-list' : 'Vos créneaux horaires déjà enregistrés sont listés ici'
+                },
+                {
+                    onBeforeStart: function(){
+                        $('#serie-add').click(function(e){
+                            enjoyhint_instance.trigger('serie-add');
+                        });
+                    },
+                    selector:'#serie-add',
+                    event:'serie-add',
+                    event_type:'custom',
+                    description:'Cliquez sur le bouton + pour ajouter un nouveau créneau horaire',
+                    shape: 'circle'
+                }
+            ];
+            break;
+        case 'serie/add':
+            var enjoyhint_script_steps = [
+                {
 
+                    'next #appbundle_serie_name' : 'Obligatoire : saisissez le nom du créneau horaire'
+                },
+                {
+                    'next #heureDebut' : 'Obligatoire : saisissez l\'heure de début du créneau horaire'
+                },
+                {
+                    'next #heureFin' : 'Obligatoire : saisissez l\'heure de fin du créneau horaire'
+                },
+                {
+                    'next #appbundle_serie_save' : 'Enregistrez votre créneau horaire'
+                },
+                {
+                    onBeforeStart: function(){
+                        $('#serie-link').click(function(e){
+                            enjoyhint_instance.trigger('previsualisation-link');
+                        });
+                    },
+                    selector:'#previsualisation-link',
+                    event:'previsualisation-link',
+                    event_type:'custom',
+                    description:'Cliquez maintenant sur le menu prévisualisation',
+                    shape: 'circle'
+                }
+            ];
+            break;
     }
 
 
